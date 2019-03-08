74959d26b7c137be6ae2fd04db2ff71e82af52c4
chenyuefang <416326391@qq.com>
2019-03-08 09:04:07 +0800

    alter 0308.sql

--- src/demo/0308.sql
+++ src/demo/0308.sql
@@ -2,7 +2,58 @@ show  full columns from db_school.student; -- 显示所有字段
 
 show full tables from db_school; -- 显示数据库两张表
 
-show variables like 'C%';
+show variables like 'C%'; --  显示变量
 
 select ENAME,HIREDATE -- 查询语句投影操作
 from scott.emp;
+
+select  distinct JOB -- distinct 去重
+from emp;
+
+select *
+from emp
+# where SAL>2000;
+# where SAL<>3000; -- 不等于
+# where SAL<>300 and JOB<> 'salesman';
+where SAL<>300 or JOB<> 'salesman';
+
+select *
+from emp
+where ENAME=binary 'Scott';
+
+select JOB, ENAME,HIREDATE
+from emp
+# order by ENAME asc ;-- ascend   上升，升序
+# order by ENAME desc ;-- descend  下降，降序
+# order by ENAME;  -- 默认升序，可不写asc
+# order by HIREDATE;     -- 工龄长的排前排后？
+order by JOB,ENAME desc ;
+
+select COMM
+from emp
+order by COMM; -- 空值认为是最小的值
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/demo/exec-1.sql
+++ src/demo/exec-1.sql
@@ -10,11 +10,11 @@ where JOB='clerk';
 # 3. 检索出奖金多于基本工资的员工信息
   select *
 from scott.emp
-where SAL>COMM;
+where COMM>SAL;
 # 4. 检索出奖金多于基本工资 30% 员工信息
   select *
 from scott.emp
-where SAL>COMM*1.3;
+where COMM=SAL*1.3;
 # 5. 希望看到 10 部门的经理或者 20 部门的职员 clerk 的信息
   select *
 from scott.emp
@@ -25,7 +25,7 @@ where DEPTNO=10 and JOB='Manger' or emp.DEPTNO=20 and JOB='Clerk';
 # 7. 找出获得奖金的员工的工作
   select JOB
 from emp
-where SAL >0;
+where COMM >0;
 # 8. 找出奖金少于 100 或者没有获得奖金的员工的信息
 select *
 from emp

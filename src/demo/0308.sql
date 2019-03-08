3566d576467a79fceffdd2f12dbe210fd32a13a4
chenyuefang <416326391@qq.com>
2019-03-08 08:44:36 +0800

    create 0308.sql

---
+++ src/demo/0308.sql
@@ -0,0 +1,8 @@
+show  full columns from db_school.student; -- 显示所有字段
+
+show full tables from db_school; -- 显示数据库两张表
+
+show variables like 'C%';
+
+select ENAME,HIREDATE -- 查询语句投影操作
+from scott.emp;

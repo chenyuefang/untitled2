8bde9bda80109e95bb7fe162ae2e669b805bb55c
chenyuefang <416326391@qq.com>
2019-03-12 10:56:08 +0800

    create 0312.sql

--- src/demo03112/0312.sql
+++ src/demo03112/0312.sql
@@ -7,8 +7,15 @@ drop index idx_ename on scott.emp;
 
 show index from db_school.student;
 
+select ENAME
+from scott.emp;
 
-create index idx_password on db_csdn.user(password);
+-- 如果 index 只有好处，没有必要存在这个概念
+create index ind_password on db_csdn.user(password); -- 434176 KB
+create index ind_username on db_csdn.user(username); -- 434176 KB
+create index ind_email on db_csdn.user(email); -- 434176 KB
+
+show index from db_csdn.user;
 
 -- 查询排名前十的密码？
 select password,count(password)
@@ -22,5 +29,14 @@ from db_csdn.user
 group by email
 order by 2 desc
 limit 10 offset 0;
+
+select *
+from db_csdn.user
+limit 100 offset 0;
+
+select password
+from db_csdn.user
+where password regexp '^\198[0-9]{1}[0-9]{1,2}[0-9]{1,2}$';
+
 
 
